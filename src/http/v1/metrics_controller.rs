8fb44ea33d119eb858d5740965bf23e6a65cf489
danielhe4rt <hey@danielheart.dev>
2024-08-16 17:50:12 -0300

    fix: linting

--- src/http/v1/metrics_controller.rs
+++ src/http/v1/metrics_controller.rs
@@ -10,6 +10,7 @@ use actix_web::{get, post, web, HttpResponse, Responder};
 use charybdis::operations::{Find, Insert};
 use charybdis::options::Consistency::One;
 use charybdis::types::Text;
+use scylla::statement::Consistency;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
@@ -58,6 +59,8 @@ pub async fn get_user_metrics(
 
   let user_metrics_by_channel = user_metrics_by_channel
     .find_by_partition_key()
+    .consistency(Consistency::LocalOne)
+    .page_size(5)
     .execute(&data.database)
     .await?
     .try_collect()
@@ -66,6 +69,7 @@ pub async fn get_user_metrics(
 
   let user_metrics_by_category = user_metrics_by_category
     .find_by_partition_key()
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?
     .try_collect()
@@ -109,26 +113,28 @@ pub async fn post_heartbeat(
 
   main_metrics
     .increment_watch_time_in_minutes(1)
-    .consistency(One)
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?;
   user_metrics_by_channel
     .increment_minutes_watched(1)
-    .consistency(One)
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?;
   user_metrics_by_category
     .increment_minutes_watched(1)
-    .consistency(One)
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?;
 
   let user_metrics_by_category = user_metrics_by_category
     .find_by_primary_key()
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?;
   let user_metrics_by_channel = user_metrics_by_channel
     .find_by_primary_key()
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?;
 
@@ -166,15 +172,18 @@ pub async fn post_heartbeat(
       current_minutes_by_channel - 1
     )
   )
+  .consistency(Consistency::LocalOne)
   .execute(&data.database)
   .await?;
 
   user_category_leaderboard
     .insert()
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?;
   user_channels_leaderboard
     .insert()
+    .consistency(Consistency::LocalOne)
     .execute(&data.database)
     .await?;
 
